3a7a7345efd3c66f9a77be6d7e6c8e8b17953c43
nubia-sm <sancheznubia816@gmail.com>
2025-05-22 12:49:11 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/temperatura", (req, res) => {
+  res.json({ valor: "10°C", timestamp: new Date().toISOString() });
+});
+
+app.listen(PORT, () => {
+  console.log(`servidor corriendo en puerto ${PORT}`);
+});
